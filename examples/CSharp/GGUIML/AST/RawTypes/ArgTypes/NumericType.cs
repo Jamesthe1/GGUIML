ff8d69d051778c49747a1aba7d1351bce906aaad
Jamesthe1 <jamesthe1sky@gmail.com>
2026-06-12 15:35:06 -0500

    Add numeric restrictions

--- examples/CSharp/GGUIML/AST/RawTypes/ArgTypes/NumericType.cs
+++ examples/CSharp/GGUIML/AST/RawTypes/ArgTypes/NumericType.cs
@@ -1,11 +1,29 @@
+using System;
+using System.Linq;
 using GGUIML.Extensions;
 
 namespace GGUIML.AST.Types {
     internal class NumericType : RawType {
+        public Type[] Restrictions { get; private set; }
+
         public override string TypeName => "numeric";
-        // TODO: Optional numeric type restriction
+
+        public NumericType () {}
+        public NumericType (Type[] restrictions) {
+            Restrictions = restrictions;
+        }
+
         public override bool CanParse (string data) {
-            return ParserExtensions.CanParse (data.ParseNumeric);
+            try {
+                INumeric result = data.ParseNumeric ();
+                if (Restrictions.Length == 0)
+                    return true;
+                
+                return Restrictions.Contains (result.GetType ());
+            }
+            catch (FormatException) {
+                return false;
+            }
         }
     }
 }
